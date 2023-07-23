46ea8102207fbcc5bb1cffe0cf18c0750a3a3b2f
rishabj <rishabj@cisco.com>
2023-07-23 13:22:58 +0530

    Initial Commit

---
+++ math_utils/utils.py
@@ -0,0 +1,9 @@
+# math_utils/utils.py
+
+def square(x):
+    """Calculate the square of a number."""
+    return x ** 2
+
+def cube(x):
+    """Calculate the cube of a number."""
+    return x ** 3
---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='math_utils',
+    version='0.1.0',
+    author='Rishab Jain',
+    author_email='rishabjain.cs@gmail.com',
+    description='A simple math utility library',
+    url='https://github.com/rishabjaincs/math_utils',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
